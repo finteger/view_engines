dcd4a36c07db11378578ca70676eb2417e3e866b
finteger <nashassociatesinc@gmail.com>
2024-11-19 18:56:42 -0700

    Update

--- app.js
+++ app.js
@@ -1,9 +1,14 @@
 const express = require('express');
 const app = express();
+require('dotenv').config();
 const PORT = process.env.PORT || 8000;
 
 app.set("views", "./views");
 app.set("view engine", "ejs");
+
+app.get("/home", (req, res)=> {
+  res.render("index");
+});
 
 app.listen(PORT, ()=>{
     console.log(`Connected to port ${PORT}`);
